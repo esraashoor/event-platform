02ceefd23daf5d6fa6079877ce934c7bf09996ac
esra <esra.ashoor.e@gmail.com>
2024-12-16 16:42:12 +0300

    commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,41 +2,20 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// Define a type for the cached object
-interface MongooseCache {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-}
-
-// Safely extend the global object with a mongoose cache
-const globalWithMongooseCache = global as typeof global & { mongoose: MongooseCache };
-
-// Use the global cache or initialize it
-const cached: MongooseCache = globalWithMongooseCache.mongoose || { conn: null, promise: null };
-
-// Assign the cache to the global object for reuse
-globalWithMongooseCache.mongoose = cached;
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn; // Return cached connection if available
-  }
+  if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URI) {
-    throw new Error('MONGODB_URI is missing');
-  }
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  if (!cached.promise) {
-    // Create a new connection promise if one doesn't already exist
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: 'Hanba',
-      bufferCommands: false,
-    });
-  }
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-  // Wait for the promise to resolve and cache the connection
   cached.conn = await cached.promise;
 
   return cached.conn;
-};
+}
 
