b8b6b614c101b09aa57e2091d4aae75c96bc4208
esra <esra.ashoor.e@gmail.com>
2024-12-25 15:12:31 +0300

    implement

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -2,7 +2,6 @@ import CheckoutButton from '@/components/shared/Checkoutbutton';
 import Collection from '@/components/shared/Collection';
 import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions';
 import { formatDateTime } from '@/lib/utils';
-import { SearchParamProps } from '@/types';
 import Image from 'next/image';
 
 // Define params and searchParams as Promises
